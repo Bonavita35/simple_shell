4a701b36631c22898873fd861e500efc8070667e
ikenwaa <ajohn9353@gmail.com>
2022-11-19 02:49:45 +0100

    Final push

--- exits.c
+++ exits.c
@@ -3,7 +3,7 @@
 /**
  **_strncpy - copies a string
  *@dest: the destination string to be copied to
- *@src: the source string
+ *@src: the source string.
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
